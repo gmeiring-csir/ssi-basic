813201ddd79429a652e4934a6f93086767fefd63
Martin Meiring <gam.meiring@gmail.com>
2022-06-30 15:56:14 +0200

    Basic setup

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-get-random-values';
+
+import type { InitConfig } from '@aries-framework/core';
+import { Agent } from '@aries-framework/core';
+import { WsOutboundTransport, HttpOutboundTransport } from '@aries-framework/core';
+
+import { agentDependencies } from '@aries-framework/react-native';
+const config: InitConfig = {
+  label: 'docs-rn-agent',
+  walletConfig: {
+    id: 'wallet-id',
+    key: 'testkey0000000000000000000000000',
+  },
+}
 
 export default function App() {
+  // const agent = new Agent(config, agentDependencies);
+  // agent.registerOutboundTransport(new WsOutboundTransport());
+  // agent.registerOutboundTransport(new HttpOutboundTransport());
+  // const initialize = async () => await agent.initialize().catch(console.error);
   return (
     <View style={styles.container}>
-      <Text>SSI DEMO!</Text>
+      <Text>SSI DEMO! </Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ metro.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    resolver: {
+      extraNodeModules: require("expo-crypto-polyfills"),
+      sourceExts: ['js', 'json', 'ts', 'tsx', 'cjs'],
+    },
+  };
